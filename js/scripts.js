57cd9c37b1046d2a1303b386688e58869ead60d3
Dana Weiss and Grady Shelton <dana@>
2017-04-05 16:02:10 -0700

    add clear subtotal upon hold click

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,6 @@ function Player(sub, grand) {
   this.grand = grand;
   this.currentRoll=0;
 }
-//
-// function SubTotal() {
-//   this.rollArray = []
-// }
-
-// SubTotal.prototype.getSub = function(rollArray){
-//   for (i=0; i<=subTotal.rollArray.length; i++) {
-//     // var newSubTotal = (subTotal.rollArray[i]);
-//     if (parseInt(rollArray[i]) >= 2) {
-//       var newSubTotal += parseInt(subTotal.rollArray[i]);
-//       console.log(newSubTotal);
-//       alert("test");
-//     }
-//   }
-// }
 
 // This prototype works
 Player.prototype.roll = function(roll) {
@@ -36,36 +21,51 @@ Player.prototype.getSub = function(){
 
 Player.prototype.getGrand = function(){
   this.grand += this.subTotal;
+  this.subTotal = 0;
 }
+
 
 // User Interface
 $(document).ready(function() {
 // Player One Start
-  //
   var sub = 0;
   var grand = 0;
   var player1 = new Player(sub, grand);
-  // var subTotal = new SubTotal();
+  var player2 = new Player(sub, grand);
 
 // Player One Roll
   $("#p1-roll").last().click(function(){
   player1.roll();
   player1.getSub();
-    console.log("in click"+ player1.subTotal);
-    // var sub = player1.getSub();
-    // subTotal.rollArray.push(rollResult);
-    // console.log(subTotal.rollArray);
-    // displaying roll correctly
-    // var rollingSubTotal = subTotal.getSub();
     $("#p1-die-result").text("");
     $("#p1-die-result").text(player1.currentRoll);
     $("#p1-subTotal").text(player1.subTotal);
-    // good stuff^^^
-  });
-// Player One Hold
-  $("#p1-hold").last().click(function(){
-    player1.getGrand();
-    $("#p1-grand").text(player1.grand);
   });
 
+// Player One Hold
+  $("#p1-hold").last().click(function(){
+    $("#p1-die-result").text("");
+    $("player1.subTotal").val(0).text("");
+    player1.getGrand();
+    $("#p1-grand").text(player1.grand);
+    $("#p1-subTotal").text("");
+  });
+
+// Player Two Roll
+  $("#p2-roll").last().click(function(){
+  player2.roll();
+  player2.getSub();
+    $("#p2-die-result").text("");
+    $("#p2-die-result").text(player2.currentRoll);
+    $("#p2-subTotal").text(player2.subTotal);
+  });
+  
+// Player Two Hold
+  $("#p2-hold").last().click(function(){
+    $("#p2-die-result").text("");
+    $("player2.subTotal").val(0).text("");
+    player2.getGrand();
+    $("#p2-grand").text(player2.grand);
+    $("#p2-subTotal").text("");
+  });
 });
