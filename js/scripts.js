d524fece652be6af5701ac4548a4f2f85542dfd1
Dana Weiss and Grady Shelton <dana@>
2017-04-05 15:29:29 -0700

    get subtotal and grand total to update score properly. heavy emphasis on this utility

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,71 @@
 // Business Logic
 
 function Player(sub, grand) {
-  this.sub = sub;
+  this.subTotal = sub;
   this.grand = grand;
+  this.currentRoll=0;
+}
+//
+// function SubTotal() {
+//   this.rollArray = []
+// }
+
+// SubTotal.prototype.getSub = function(rollArray){
+//   for (i=0; i<=subTotal.rollArray.length; i++) {
+//     // var newSubTotal = (subTotal.rollArray[i]);
+//     if (parseInt(rollArray[i]) >= 2) {
+//       var newSubTotal += parseInt(subTotal.rollArray[i]);
+//       console.log(newSubTotal);
+//       alert("test");
+//     }
+//   }
+// }
+
+// This prototype works
+Player.prototype.roll = function(roll) {
+  this.currentRoll = Math.floor((Math.random() * 6) + 1);
 }
 
-// function subtotal(roll)
+Player.prototype.getSub = function(){
+  if (this.currentRoll >= 2) {
+    this.subTotal += this.currentRoll;
+  } else {
+    this.subTotal = 0;
+  }
+}
 
-Player.prototype.roll = function(roll) {
-  console.log(Math.floor((Math.random() * 6) + 1))
-  return Math.floor((Math.random() * 6) + 1);
+Player.prototype.getGrand = function(){
+  this.grand += this.subTotal;
 }
 
 // User Interface
 $(document).ready(function() {
+// Player One Start
+  //
+  var sub = 0;
+  var grand = 0;
+  var player1 = new Player(sub, grand);
+  // var subTotal = new SubTotal();
+
 // Player One Roll
   $("#p1-roll").last().click(function(){
-    var sub = 0;
-    var grand = 0;
-    var player1 = new Player(sub, grand);
-    var rollResult = player1.roll();
+  player1.roll();
+  player1.getSub();
+    console.log("in click"+ player1.subTotal);
+    // var sub = player1.getSub();
+    // subTotal.rollArray.push(rollResult);
+    // console.log(subTotal.rollArray);
+    // displaying roll correctly
+    // var rollingSubTotal = subTotal.getSub();
     $("#p1-die-result").text("");
-    $("#p1-die-result").text(rollResult);
+    $("#p1-die-result").text(player1.currentRoll);
+    $("#p1-subTotal").text(player1.subTotal);
+    // good stuff^^^
   });
 // Player One Hold
   $("#p1-hold").last().click(function(){
-    $("#p1-grand").text("turn total");
-  });
-
-// Player Two Roll
-  $("#p2-roll").last().click(function(){
-    var sub = 0;
-    var grand = 0;
-    var player2 = new Player(sub, grand);
-    var rollResult2 = player2.roll();
-    $("#p2-die-result").text("");
-    $("#p2-die-result").text(rollResult2);
-  });
-
-// Player Two Hold
-  $("#p2-hold").last().click(function(){
-    $("#p2-grand").text("turn total");
+    player1.getGrand();
+    $("#p1-grand").text(player1.grand);
   });
 
 });
